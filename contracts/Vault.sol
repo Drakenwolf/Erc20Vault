c5f44cf128e5b60456e6067c9c46491ec4c29e9f
Drakenwolf <you@example.comhans.haar6@gmail.com>
2022-04-09 16:44:52 -0600

    feature: adding allowance functions

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,45 +2,15 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Vault {
-    address _owner;
     address tokenAddress;
 
     constructor(address _tokenAddress) {
-        _owner = msg.sender;
         tokenAddress = _tokenAddress;
     }
-    
-
-    modifier isOwner {
-        require(msg.sender == _owner);
-        _;
-    }
-
-    event successfullyChangedOwner(address from, address to);
-
-    function transferOwnerShip(address addr) public isOwner {
-        _owner = addr;
-        emit successfullyChangedOwner(msg.sender, addr);
-    }
-    
-    // modifier checkAddress(address addr) {
-    //     require(addr != address(0), "Token address must not be zero");
-    //     require(isContract(addr), "Address must be of contract");
-    //     _;        
-    // }   
-
-    function isContract(address addr) private view returns(bool) {
-        uint codeLength;
-        assembly {
-            codeLength := extcodesize(addr)
-        }
-        return codeLength == 0 ? false : true;
-    }
-
+     
     // userAddress => tokenAddress => token amount
     mapping (address => mapping (address => uint256)) userTokenBalance;
 
@@ -48,7 +18,9 @@ contract Vault {
 
     function depositToken( uint256 amount) public  {
         require(IERC20(tokenAddress).balanceOf(msg.sender) >= amount, "Your token amount must be greater then you are trying to deposit");
+        require(IERC20(tokenAddress).approve(address(this), amount));
         require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount));
+
         userTokenBalance[msg.sender][tokenAddress] += amount;
         emit tokenDepositComplete(tokenAddress, amount);
     }
@@ -56,7 +28,7 @@ contract Vault {
     event tokenWithdrawalComplete(address tokenAddress, uint256 amount);
 
     function withDrawAll() public {
-        require(userTokenBalance[msg.sender][tokenAddress] >= 0);
+        require(userTokenBalance[msg.sender][tokenAddress] > 0, "User doesnt has funds on this vault");
         uint256 amount = userTokenBalance[msg.sender][tokenAddress];
         IERC20(tokenAddress).transfer(msg.sender, amount);
         userTokenBalance[msg.sender][tokenAddress] = 0;
@@ -65,6 +37,7 @@ contract Vault {
 
     function withDrawAmount(uint256 amount) public {
         require(userTokenBalance[msg.sender][tokenAddress] >= amount);
+        IERC20(tokenAddress);
         IERC20(tokenAddress).transfer(msg.sender, amount);
         userTokenBalance[msg.sender][tokenAddress] -= amount;
         emit tokenWithdrawalComplete(tokenAddress, amount);
